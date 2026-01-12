656cb3e93d38cb30a09d2a9b4f034daf62e49243
ssoto-su <ssoto-su@student.42malaga.com>
2026-01-12 21:10:40 +0100

    agregue un par de lineas en la funcion validator

--- src/validator.c
+++ src/validator.c
@@ -6,12 +6,14 @@
 /*   By: ssoto-su <ssoto-su@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 18:08:15 by carmegon          #+#    #+#             */
-/*   Updated: 2026/01/12 20:04:05 by ssoto-su         ###   ########.fr       */
+/*   Updated: 2026/01/12 21:10:14 by ssoto-su         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
+//Iterar en la lista y verificar todas las condiciones posible para
+// que se pueda tokenizar. 
 int	validator(char *input)
 {
 	int		i;
@@ -24,7 +26,12 @@ int	validator(char *input)
 	i = 0;
 	while (input[i])
 	{
-		if (input[0] == '"' && status)
+		//añadir un is space
+		if (input[i] == 32 || (input[i] >= 9 && input[i] <= 13))
+			i++;
+		if (input[0] == '|')
+			status = '|';
+		else if (input[0] == '"' && status)
 			status = '"';
 		else if (input[0] == '\'')
 			status = '\'';
