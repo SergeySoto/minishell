7fe96743653109bb1ef1d187e8a14948287e80a7
ssoto-su <ssoto-su@student.42malaga.com>
2026-01-15 20:34:43 +0100

    funcion check_redirect implementada. Se han fucionado las funciones que buscan caracteres dobles invalidos

--- src/validator.c
+++ src/validator.c
@@ -6,33 +6,51 @@
 /*   By: ssoto-su <ssoto-su@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/12 18:08:15 by carmegon          #+#    #+#             */
-/*   Updated: 2026/01/15 17:42:23 by ssoto-su         ###   ########.fr       */
+/*   Updated: 2026/01/15 20:30:10 by ssoto-su         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 /*Idea: create a function that looks for the right heredoc and if there is
-something an < after de dobule << return 0 else return 1. Check that logic*/
+something after de dobule << return 0 else return 1. Check that logic*/
 
-static int	check_mid_double_pipes(char *str)
+static void	update_quote_status(char c, char *quotes)
+{
+	if ((c == '"' || c == '\'') && *quotes == 0)
+		*quotes = c;
+	else if (c == *quotes)
+		*quotes = 0;
+}
+
+static int	invalid_neighbor(char c)
+{
+	if (c == '|' || c == '<' || c == '>' || c == '\0')
+		return (1);
+	return (0);
+}
+
+static int	check_redirect(char *str)
 {
 	int		i;
-	char	quotes;
+	int		j;
+	char	quote;
 
-	quotes = 0;
 	i = 0;
+	quote = 0;
 	while (str[i])
 	{
-		if ((str[i] == '"' || str[i] == '\'') && quotes == 0)
-			quotes = str[i];
-		else if (str[i] == quotes)
-			quotes = 0;
-		if (quotes == 0)
+		update_quote_status(str[i], &quote);
+		if ((str[i] == '<' || str[i] == '>') && quote == 0)
 		{
-			if (str[i] == '|' && str[i + 1] == '|')
+			j = i + 1;
+			if (str[i + 1] == str[i])
+				j++;
+			while (str[j] && is_space(str[j]))
+				j++;
+			if (invalid_neighbor(str[j]))
 			{
-				printf("Error: Syntax error near unexpected token `||'\n");
+				printf("Error: Syntax error near unexpected token\n");
 				return (0);
 			}
 		}
@@ -41,7 +59,7 @@ static int	check_mid_double_pipes(char *str)
 	return (1);
 }
 
-static int	check_mid_double_ampersand(char *str)
+static int	check_invalid_double(char *str)
 {
 	int		i;
 	char	quotes;
@@ -50,12 +68,14 @@ static int	check_mid_double_ampersand(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if ((str[i] == '"' || str[i] == '\'') && quotes == 0)
-			quotes = str[i];
-		else if (str[i] == quotes)
-			quotes = 0;
+		update_quote_status(str[i], &quotes);
 		if (quotes == 0)
 		{
+			if (str[i] == '|' && str[i + 1] == '|')
+			{
+				printf("Error: Syntax error near unexpected token `||'\n");
+				return (0);
+			}
 			if (str[i] == '&' && str[i + 1] == '&')
 			{
 				printf("Error: Syntax error near unexpected token `&&'\n");
@@ -71,15 +91,15 @@ int	validator(char *input)
 {
 	if (!input)
 		return (0);
-	if (!check_pipe(input))
-		return (0);
 	if (!check_quotes(input))
+		return (0);
+	if (!check_pipe(input))
 		return (0);
 	if (!check_pending_pipe(input))
 		return (0);
-	if (!check_mid_double_pipes(input))
+	if (!check_redirect(input))
 		return (0);
-	if (!check_mid_double_ampersand(input))
+	if (!check_invalid_double(input))
 		return (0);
 	if (!check_forbidden(input))
 		return (0);
