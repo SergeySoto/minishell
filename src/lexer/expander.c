2b449212ca367922e15410451ce05564399b3c9e
ssoto-su <ssoto-su@student.42malaga.com>
2026-01-21 19:58:02 +0100

    Implementado heredoc_bf_dollar: actualiza la variable expand de ser necesario

--- src/lexer/expander.c
+++ src/lexer/expander.c
@@ -6,7 +6,7 @@
 /*   By: ssoto-su <ssoto-su@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/20 20:12:38 by ssoto-su          #+#    #+#             */
-/*   Updated: 2026/01/21 17:46:07 by ssoto-su         ###   ########.fr       */
+/*   Updated: 2026/01/21 19:43:43 by ssoto-su         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@ void	expand_checker(t_token *lst)
 
 	if (!lst || !lst->content)
 		return ;
-	quote = 0;
 	i = 0;
+	quote = 0;
 	while (lst->content[i])
 	{
 		update_quote_status(lst->content[i], &quote);
@@ -31,4 +31,12 @@ void	expand_checker(t_token *lst)
 		}
 		i++;
 	}
+}
+
+void	heredoc_bf_dollar(t_token *lst)
+{
+	if (!lst || !lst->content)
+		return ;
+	if (lst->type == 4 && lst->next->expand == 1)
+		lst->next->expand = 0;
 }
