50d08c30d853eac6afd3d5ce516cfcd1716e8e85
ssoto-su <ssoto-su@student.42malaga.com>
2026-02-06 16:43:36 +0100

    me hjan hechadooooooo!!!

--- src/executor/executor.c
+++ src/executor/executor.c
@@ -17,22 +17,22 @@ void	get_cmd_path(t_mini *mini)
 			path_splited = ft_split(env->value, ':');
 			if (!path_splited)
 				printf("Environment path not found\n");
-			else
-				find_command(path_splited, &(*mini).cmds);
-			free_token(NULL, path_splited);
 			break ;
 		}
 		env = env->next;
 	}
+	find_command2(path_splited, &(*mini).cmds);
+	free_token(NULL, path_splited);
 	return ;
 }
 
+/*
 void	find_command(char **path_env, t_cmd **cmd)
 {
 	char	*aux;
 	char	*full_path;
 	int		i;
-
+	
 	i = 0;
 	full_path = NULL;
 	while (path_env[i])
@@ -52,4 +52,35 @@ void	find_command(char **path_env, t_cmd **cmd)
 		i++;
 	}
 	return ;
+}
+*/
+
+void	find_command2(char **path_env, t_cmd **cmd)
+{
+	t_cmd	*current;
+	char	*aux;
+	char	*full_path;
+	int		i;
+
+	full_path = NULL;
+	aux = NULL;
+	current = (*cmd);
+	while (current)
+	{
+		i = 0;
+		while (path_env[i])
+		{
+			aux = ft_strjoin(path_env[i], "/");
+			full_path = ft_strjoin(aux, current->args[0]);
+			free(aux);
+			if (access(full_path, X_OK) == 0)
+			{
+				current->cmd_path = full_path;
+				break ;
+			}
+			free(full_path);
+		}
+		i++;
+		current = current->next;
+	}
 }
