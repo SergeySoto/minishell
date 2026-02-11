5b427df7ba72c7629bb78e5d890eb18a9605f1b9
ssoto-su <ssoto-su@student.42malaga.com>
2026-02-11 16:56:57 +0100

    Maria esta molesta. Disculpate

--- src/executor/redirect_utils.c
+++ src/executor/redirect_utils.c
@@ -1,16 +1,31 @@
 
 #include "../../includes/minishell.h"
 
-void	save_std_fds(t_mini *mini)
+int	save_std_fds(t_mini *mini)
 {
 	mini->stdin_backup = dup(STDIN_FILENO);
+	if (mini->stdin_backup < 0)
+		return (perror("Dup failed"), -1);
 	mini->stdout_backup = dup(STDOUT_FILENO);
+	if (mini->stdout_backup < 0)
+	{
+		close(mini->stdin_backup);
+		return (perror("Dup failed"), -1);
+	}
+	return (0);
 }
 
-void	restore_std_fds(t_mini *mini)
+int	restore_std_fds(t_mini *mini)
 {
-	dup2(mini->stdin_backup, STDIN_FILENO);
-	dup2(mini->stdout_backup, STDOUT_FILENO);
+	if (dup2(mini->stdin_backup, STDIN_FILENO) < 0)
+		return (perror("Dup failed"), -1);
+	if (dup2(mini->stdout_backup, STDOUT_FILENO) < 0)
+	{
+		close(mini->stdin_backup);
+		close(mini->stdout_backup);
+		return (perror("Dup failed"), -1);
+	}
 	close(mini->stdin_backup);
 	close(mini->stdout_backup);
+	return (0);
 }
