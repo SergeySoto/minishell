1d6b6b77056ec8ed608124aad1d6322292931ba0
carmegon <carmegon@student.42malaga.com>
2026-03-03 19:14:04 +0100

    hoy no toque nada de codigo :)

--- src/builtins/utils_exit.c
+++ src/builtins/utils_exit.c
@@ -7,17 +7,32 @@ int	is_numeric_av(char *str)
 	int	i;
 
 	i = 0;
+	if (!str || !*str)
+		return (0);
+	while (str[i] && is_space(str[i]))
+		i++;
 	while (str[i])
 	{
-		if (is_space(str[i]))
-			continue ;
+		if (is_space(str[i]) && ft_isdigit(str[i + 1]))
+			return (0);
 		else if ((str[i] == '-' || str[i] == '+') && !ft_isdigit(str[i + 1]))
 			return (0);
 		else if (!ft_isdigit(str[i]))
 			return (0);
 		else if (ft_isdigit(str[i]) && ((str[i + 1] == '-' || str[i + 1] == '+')))
 			return (0);
+		else if (str[i] && is_space(str[i]))
+		{
+			i++;
+			continue ;
+		}
 		i++;
 	}
 	return (1);
+}
+
+void	ft_exit(t_cmd *cmd)
+{
+	if (!cmd->args || !*cmd->args)
+		return (0);
 }
