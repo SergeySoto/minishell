f0a6d1bf1f36eb794cd5528ad9db6c9a70107747
Carmen González <carmengh106@gmail.com>
2026-03-09 11:07:02 +0100

    tengo el overfow, pero tengo que cambiar la logica

--- includes/minishell.h
+++ includes/minishell.h
@@ -38,6 +38,7 @@ void		swap_nodes(t_mini *mini);
 void		print_order_env(t_mini *mini);
 int			order_env(t_mini *mini);
 int			ft_export(t_mini *mini, char **av);
+int         count_av(char **av);
 int			ft_cd(t_mini *mini, char **av);
 int			is_numeric_av(char *str);
 long long	ft_atoll(char *str);
--- src/builtins/utils_exit.c
+++ src/builtins/utils_exit.c
@@ -28,9 +28,9 @@ int	is_numeric_av(char *str)
 
 long long	ft_atoll(char *str)
 {
-	int		i;
-	int		sign;
-	long	n;
+	int			i;
+	int			sign;
+	long long	n;
 
 	i = 0;
 	while (str[i] && is_space(str[i]))
@@ -47,24 +47,45 @@ long long	ft_atoll(char *str)
 	{
 		if (n > LLONG_MAX || n < LLONG_MIN)
 			return (LLONG_MAX);
-/* 		else if (str[i] < '0' || str[i] > '9')
-			return (n * sign); */
+		else if (is_overflow_av(str[i], sign, n)
+			return (LLONG_MAX);
 		n = (n * 10) + (str[i] - '0');
 		i++;
 	}
 	return (n * sign);
 }
 
-int	is_valid_av(char **av)
+int	is_overflow_av(char c, int sign, long long n)
 {
-	int	i;
-
-	i = 0;
-	while (av[i])
+	if (LLONG_MAX / 10 < n)
+		return (1);
+	else if ((n == LLONG_MAX / 10) && ((sign == 1) && (c >= '8')))
+		return (1);
+	else if ((n == LLONG_MAX / 10) && ((sign == -1) && (c == '9')))
+		return (1);
+	else
+		return (0);
 }
 
-void	ft_exit(t_cmd *cmd)
+void	ft_exit(t_mini *mini, t_cmd *cmd)
 {
-	if (!cmd->args || !*cmd->args)
-		return ;
+	int			av;
+	long long	n;
+
+	ft_putstr_fd("exit\n", 2);
+	av = count_av(cmd->args);
+	if (av == 1)
+	{
+		free_struct_mini(mini);
+		exit(0);
+	}
+	else if (av == 2 && is_numeric_av(cmd->args[1]))
+	{
+		if (ft_atoll(cmd->args[1]))
+		
+		n = ft_atoi(cmd->args[1]);
+		mini->exit_status = n;
+		free_struct_mini(mini);
+		exit(n);
+	}
 }
