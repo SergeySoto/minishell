417ad9bd9dba6ec9f4bb153f119eaf20ae2d38d9
ssoto-su <ssoto-su@student.42malaga.com>
2026-03-11 18:59:15 +0100

    feat: refactorization of heredoc logic

--- src/parser/heredoc.c
+++ src/parser/heredoc.c
@@ -1,6 +1,48 @@
 
 #include "../../includes/minishell.h"
 
+static int	hd_signal_cleanup(int fd, t_mini *mini)
+{
+	close(fd);
+	unlink(".minishell_heredoc");
+	dup2(mini->stdin_backup, STDIN_FILENO);
+	close(mini->stdin_backup);
+	set_signals_interactive();
+	return (-1);
+}
+
+static int	hd_eof_warning(char *delimiter)
+{
+	ft_putstr_fd(ERROR_HD, 2);
+	ft_putstr_fd(delimiter, 2);
+	ft_putstr_fd("\n", 2);
+	return (1);
+}
+
+static int	hd_read_loop(int fd,char *delimiter, t_mini *mini)
+{
+	char *line;
+	while (1)
+	{
+		line = readline("> ");
+		if (g_signal != 0)
+		{
+			free(line);
+			return (hd_signal_cleanup(fd, mini));
+		}
+		if (!line)
+			return (hd_eof_warning(delimiter));
+		if (ft_strcmp(line, delimiter) == 0)
+		{
+			free(line);
+			return (1);
+		}
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
+		free(line);
+	}
+}
+
 int	handle_heredoc(char *delimiter, t_mini *mini)
 {
 	int		fd;
@@ -13,35 +55,8 @@ int	handle_heredoc(char *delimiter, t_mini *mini)
 	mini->stdin_backup = dup(STDIN_FILENO);
 	g_signal = 0;
 	set_signals_heredoc();
-	while (1)
-	{
-		line = readline("> ");
-		if (g_signal != 0)
-		{
-			free(line);
-			close(fd);
-			unlink(".minishell_heredoc");
-			dup2(mini->stdin_backup, STDIN_FILENO);
-			close(mini->stdin_backup);
-			set_signals_interactive();
-			return (-1);
-		}
-		if (!line)
-		{
-			ft_putstr_fd(ERROR_HD, 2);
-			ft_putstr_fd(delimiter, 2);
-			ft_putstr_fd("\n", 2);
-			break ;
-		}
-		if (ft_strcmp(line, delimiter) == 0)
-		{
-			free(line);
-			break ;
-		}
-		write(fd, line, ft_strlen(line));
-		write(fd, "\n", 1);
-		free(line);
-	}
+	if (hd_read_loop(fd, delimiter, mini) == -1)
+		return (-1);
 	close(mini->stdin_backup);
 	close(fd);
 	fd = open(".minishell_heredoc", O_RDONLY);
