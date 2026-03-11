3b7197efafc68a3916b223574a0b147b55fcdd9b
ssoto-su <ssoto-su@student.42malaga.com>
2026-03-11 19:06:04 +0100

    feat: refactorization in split logic

--- src/lexer/split_utils_wc.c
+++ src/lexer/split_utils_wc.c
@@ -14,10 +14,10 @@ static void	count_ops(char *str, int *i, int *count, char *in_word)
 		(*i)++;
 }
 
-static void	count_regular(char *str, int i, int *count, char *in_w, char *in_q)
+static void	count_regular(char c, int *count, char *in_w, char *in_q)
 {
-	update_quote_status(str[i], in_q);
-	if (is_space(str[i]) && *in_q == 0)
+	update_quote_status(c, in_q);
+	if (is_space(c) && *in_q == 0)
 		*in_w = 0;
 	else if (*in_w == 0)
 	{
@@ -42,7 +42,7 @@ int	word_count(char *str)
 		if (in_quotes == 0 && is_op(str[i]))
 			count_ops(str, &i, &count, &in_word);
 		else
-			count_regular(str, i, &count, &in_word, &in_quotes);
+			count_regular(str[i], &count, &in_word, &in_quotes);
 		i++;
 	}
 	return (count);
